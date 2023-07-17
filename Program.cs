44501e159c5372e30b0564ea78fa67e745defff5
Gabriel Torres <playerjogador10@outlook.com>
2023-07-17 20:51:07 -0300

    feat: read all users

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dapper.Contrib.Extensions;
+using DapperBlog.Models;
+using Microsoft.Data.SqlClient;
+
+public class Program 
+{
+  private const string CONNECTION_STRING = "Server=localhost,1433;Database=bLOG;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True";
+
+  static void Main (string[] args) 
+  {
+    ReadUsers();
+  }
+
+  public static void ReadUsers() 
+  {
+    using (var connection = new SqlConnection(CONNECTION_STRING)) 
+    {
+      connection.Open();
+      var users = connection.GetAll<User>();
+      foreach (var user in users) 
+      {
+        Console.WriteLine(@$"ID: {user.Id} // Name: {user.Name} // Email: {user.Email}");
+      }
+    }
+
+  }
+
+}
