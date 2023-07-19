7a4437358f2ab0f462d45f74c55fae52e75870c0
Gabriel Torres <playerjogador10@outlook.com>
2023-07-18 21:57:51 -0300

    feat: deletes user Repository

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -29,5 +29,19 @@ namespace DapperBlog.Repositories
         _connection.Update<User>(user);
     }
 
+    public void Delete(User user)
+    {
+      if(user.Id != 0)
+        _connection.Delete<User>(user);
+    }
+
+    public void Delete(int id)
+    {
+      if(id != 0)
+      return;
+      var user = _connection.Get<User>(id);
+      _connection.Delete<User>(user);
+    }
+
   }
 }
