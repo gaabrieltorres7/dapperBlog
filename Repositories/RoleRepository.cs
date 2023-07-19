559230910b94c18d8e042d332e4b035d2f3f35b4
Gabriel Torres <playerjogador10@outlook.com>
2023-07-18 21:21:23 -0300

    feat: RoleRepository, get all

---
+++ Repositories/RoleRepository.cs
@@ -0,0 +1,18 @@
+using Dapper.Contrib.Extensions;
+using DapperBlog.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DapperBlog.Repositories
+{
+  public class RoleRepository
+  {
+    private readonly SqlConnection _connection;
+
+    public RoleRepository(SqlConnection _connection)
+      => this._connection = _connection;
+
+    public IEnumerable<Role> GetAll()
+      => _connection.GetAll<Role>();
+      
+  }
+}
