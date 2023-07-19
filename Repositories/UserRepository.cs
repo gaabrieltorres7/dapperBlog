944d0cb178a41ea1924633191a3697456e53c8df
Gabriel Torres <playerjogador10@outlook.com>
2023-07-18 21:37:36 -0300

    feat: create user Repository

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -16,6 +16,9 @@ namespace DapperBlog.Repositories
     
     public User Get(int id)
       => _connection.Get<User>(id);
+    
+    public void Create(User user)
+      => _connection.Insert<User>(user);
 
    }
 }
